c42555d4affef30e4f0c5efbfa65d4ab6e369f4d
Dinesh-Gautam <dineshgautam5252@gmail.com>
2024-06-20 16:12:37 +0530

    styles refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import "./styles/App.css";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { StoreProvider } from "./contexts/Store";
 import { lazy } from "react";
