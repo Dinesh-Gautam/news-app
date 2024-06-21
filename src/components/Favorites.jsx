00c3f24d3fbe617163d76c5e7d3a1377dc775ff6
Dinesh-Gautam <dineshgautam5252@gmail.com>
2024-06-21 23:49:46 +0530

    fix: added nothing message

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -4,12 +4,11 @@ import { Article } from "./Article";
 import { Layout } from "./Layout";
 import { Header } from "./header/Header";
 import Nothing from "./Nothing";
+import { isEmpty } from "../utils/common";
 
 function Favorites() {
   const { favorites } = useFavorite();
   const articles = Object.values(favorites);
-
-  if (!articles) return <Nothing message="Nothing in Favorites" />;
 
   return (
     <>
@@ -17,6 +16,7 @@ function Favorites() {
         <h1>Favorites</h1>
       </Header>
       <main>
+        {isEmpty(articles) && <Nothing message="Nothing in Favorites" />}
         <Layout.FlexContainer>
           {articles.map(({ data }) => (
             <Article key={data.uri} {...data} />
